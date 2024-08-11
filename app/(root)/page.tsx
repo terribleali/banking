a01414f8cd34d81e20b8aebd40d20cd30e48af3a
terribleali <terribleali1967@gmail.com>
2024-08-11 10:53:53 +0300

    commit2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Bitch' };
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
+           />
+          welcome, bitch
+        </header>
+      </div>
+    </section>
   )
 }
 
